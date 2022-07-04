eaa6c01765c4d158055a1dfe4255f2af2357539c
Taekyun Kim <taekyunk@gmail.com>
2022-07-04 13:02:05 -0400

    add: train and score script

---
+++ 01_train.py
@@ -0,0 +1,149 @@
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_percentage_error as mape
+
+# pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from category_encoders import TargetEncoder
+
+# local modules
+from pipe_util import FactorLumpProp, FactorLumpN
+from pipe_util import read_cp, write_cp
+from pipe_util import find_outlier
+from pipe_util import find_prop
+from pipe_util import find_lift
+
+# variable importance
+from feature_importance import FeatureImportance
+
+
+# load data --------------------------------------------------------------------
+# sample data
+import seaborn as sns
+
+df = sns.load_dataset('diamonds')
+df
+df.info()
+
+
+# remove outlier ---------------------------------------------------------------
+
+idx = find_outlier(df['price'], cutoff=3)
+df = df[~idx]
+
+
+# split data -------------------------------------------------------------------
+
+x_train, x_test, y_train, y_test = train_test_split(
+    df.drop(['price'], axis='columns'),
+    df['price'],
+    test_size = 0.2,
+    random_state = 123
+)
+
+x_train
+x_train.info()
+
+
+# build pipeline 
+
+
+# group features by treatment
+var_numeric = ['depth', 'table', 'carat']
+var_categorical_group = ['color', 'clarity']
+var_categorical_target = ['cut']
+# note that this is *not* a list
+dependent_variable = 'price'
+
+numeric_transformer = Pipeline(
+    steps = [
+        ('simple_imputer', SimpleImputer(strategy = 'mean')), 
+        ('scaler', StandardScaler())
+    ]
+)
+
+categorical_transformer_group = Pipeline(
+    steps = [
+        ('fct_lump', FactorLumpN(top_n = 3)), 
+        ('one_hot_encoder', OneHotEncoder())
+    ]
+)
+
+categorical_transformer_target = Pipeline(
+    steps = [
+        # use recommended settings from 
+        # https://github.com/scikit-learn-contrib/category_encoders/issues/327
+        ('target_encoder', TargetEncoder(min_samples_leaf=20, smoothing=10))
+    ]
+)
+
+preprocessor = ColumnTransformer(
+    transformers = [
+        ('num', numeric_transformer, var_numeric),
+        ('cat_group', categorical_transformer_group, var_categorical_group),
+        ('cat_target', categorical_transformer_target, var_categorical_target),
+    ]
+)
+
+pipe = Pipeline(
+    steps = [
+        ('preprocessor', preprocessor),
+        ('rf', RandomForestRegressor())
+    ]
+)
+
+# find parameter names
+pipe.get_params().keys()
+
+# note the prefix `rf__` with two _
+param_grid = {
+    'rf__max_depth': [2, 4, 6],
+    'rf__min_samples_split': [2, 3, 4],
+    'rf__n_estimators': [100, 120]
+}
+
+cv = GridSearchCV(pipe, param_grid, n_jobs = 2)
+cv.fit(x_train, y_train)
+
+y_test_pred = cv.predict(x_test)
+y_test_pred
+
+# compare the two for overfitting
+cv.best_score_
+r2_score(y_test, y_test_pred)
+
+mape(y_test, y_test_pred)
+
+# lift
+find_lift(y_test, y_test_pred)
+# find_lift(y_test, y_test_pred, n_bins = 3)
+
+
+# select the best model --------------------------------------------------------
+
+# select best model as a pipeline
+best_model = cv.best_estimator_
+best_model
+
+# variable importance
+fi = FeatureImportance(best_model)
+fi
+fi.get_selected_features()
+
+(fi.get_feature_importance()
+    .to_frame('imp')
+    .reset_index()
+    .sort_values('imp', ascending=False)
+)
+
+# save best model
+write_cp(best_model, 'best_model.pkl') 
---
+++ 02_score.py
@@ -0,0 +1,59 @@
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_percentage_error as mape
+
+# pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from category_encoders import TargetEncoder
+
+# local modules
+from pipe_util import FactorLumpProp, FactorLumpN
+from pipe_util import read_cp, write_cp
+from pipe_util import find_outlier
+from pipe_util import find_prop
+from pipe_util import find_lift
+
+# variable importance
+from feature_importance import FeatureImportance
+
+#-------------------------------------------------------------------------------
+
+# get some data set to score
+import seaborn as sns
+df = sns.load_dataset('diamonds')
+df
+
+# randomly select some data 
+# assume that there is a id field
+df_new = (
+    df
+    .assign(id = range(df.shape[0]))
+    .sample(n=200, random_state=123)
+    .reset_index()
+)
+df_new
+df_new.info()
+
+
+# load model
+model = read_cp('best_model.pkl')
+
+# score
+y_pred = model.predict(df_new)
+y_pred
+
+
+# combine with id
+df_pred = pd.DataFrame({'id': df_new['id'], 'pred': y_pred})
+df_pred
+
+# write to table
