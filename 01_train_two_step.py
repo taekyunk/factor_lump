61ae50818286dd353e6324d81f4877b248108af6
Taekyun Kim <taekyunk@gmail.com>
2022-07-09 17:44:26 -0400

    add: separate transformer and model

---
+++ 01_train_two_step.py
@@ -0,0 +1,188 @@
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_percentage_error as mape
+
+# pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from category_encoders import TargetEncoder
+
+# local modules
+# OneHotEncoder() will have max_categories and min_frequency with a newer version
+from pipe_util import FactorLumpProp
+from pipe_util import FactorLumpN
+from pipe_util import DropHighlyCorrelated
+from pipe_util import read_cp
+from pipe_util import write_cp
+from pipe_util import find_outlier
+from pipe_util import find_prop
+from pipe_util import find_lift
+
+# variable importance
+from feature_names import get_feature_names
+
+from plotnine import ggplot, aes, geom_point, geom_line, facet_wrap
+
+# load data --------------------------------------------------------------------
+# sample data from seaborn
+# import seaborn as sns
+
+# df = sns.load_dataset('diamonds')
+# df.to_csv('diamonds2.csv')
+df = pd.read_csv('diamonds.csv')
+df
+df.info()
+
+
+# remove outlier ---------------------------------------------------------------
+
+idx = find_outlier(df['price'], cutoff=3)
+df = df[~idx]
+
+# drop correlated --------------------------------------------------------------
+
+drop_correlated = DropHighlyCorrelated(
+    threshold=0.85, 
+    # select numeric variables here
+    candidate = ['depth', 'table', 'carat', 'x', 'y', 'z'])
+df = drop_correlated.fit_transform(df)
+df
+# dropped features
+drop_correlated.correlated_features
+
+# split data -------------------------------------------------------------------
+
+x_train, x_test, y_train, y_test = train_test_split(
+    df.drop(['price'], axis='columns'),
+    df['price'],
+    test_size = 0.2,
+    random_state = 123
+)
+
+x_train
+x_train.info()
+
+
+# build pipeline ---------------------------------------------------------------
+
+# group features by treatment
+var_numeric = ['depth', 'table', 'carat']
+var_categorical_group = ['clarity']
+var_categorical_binary = ['color']
+var_categorical_target = ['cut']
+
+# build one transformer by feature group
+numeric_transformer = Pipeline(
+    steps = [
+        # to impute with mode, use strategy='most_frequent'
+        ('simple_imputer', SimpleImputer(strategy = 'mean')), 
+        ('scaler', StandardScaler())
+    ]
+)
+
+categorical_transformer_group = Pipeline(
+    steps = [
+        ('fct_lump', FactorLumpN(top_n = 3)), 
+        ('one_hot_encoder', OneHotEncoder(drop='if_binary'))
+    ]
+)
+
+categorical_transformer_binary = Pipeline(
+    steps = [
+        ('as_binary', FunctionTransformer(lambda x: (x == 'G').astype(int))), 
+    ]
+)
+categorical_transformer_target = Pipeline(
+    steps = [
+        # use recommended settings from 
+        # https://github.com/scikit-learn-contrib/category_encoders/issues/327
+        ('target_encoder', TargetEncoder(min_samples_leaf=20, smoothing=10))
+    ]
+)
+
+# combine transformers into ColumnTransformer
+preprocessor = ColumnTransformer(
+    transformers = [
+        ('num', numeric_transformer, var_numeric),
+        ('cat_group', categorical_transformer_group, var_categorical_group),
+        ('cat_binary', categorical_transformer_binary, var_categorical_binary),
+        ('cat_target', categorical_transformer_target, var_categorical_target),
+    ]
+)
+
+# fit preprocessor only --------------------------------------------------------
+# fit processor separately to reduce time
+# put this in a pipeline to use FeatureImportance
+pp = preprocessor
+x_train_mat = pp.fit_transform(x_train, y_train)
+
+get_feature_names(pp)
+
+# CV ---------------------------------------------------------------------------
+
+param_grid = {
+    'max_features': ['sqrt', 'log2'],
+    'n_estimators': [100, 120, 140],
+}
+
+cv = GridSearchCV(RandomForestRegressor(), param_grid, n_jobs = 2, verbose=2)
+cv.fit(x_train_mat, y_train)
+
+# select the best model --------------------------------------------------------
+
+x_test_mat = preprocessor.transform(x_test)
+y_test_pred = cv.predict(x_test_mat)
+y_test_pred
+
+# compare the two for overfitting
+cv.best_score_
+r2_score(y_test, y_test_pred)
+
+mape(y_test, y_test_pred)
+
+# lift
+dfs = find_lift(y_test, y_test_pred)
+dfs['idx'] = range(dfs.shape[0])
+dfs
+
+dfst = dfs.melt(id_vars = ['idx'], value_vars = ['y_true_mean', 'y_score_mean'])
+dfst
+
+(ggplot(dfst, aes('idx', 'value', color = 'variable'))
++ geom_point()
++ geom_line()
+)
+
+(ggplot(dfst, aes('idx', 'value', color = 'variable'))
++ geom_point()
++ geom_line()
++ facet_wrap('~ variable')
+)
+
+# variable importance
+
+dfi = (
+    pd.DataFrame({
+        'name': get_feature_names(pp),
+        'importance': cv.best_estimator_.feature_importances_
+    })
+    .sort_values('importance', ascending=False)
+)
+dfi
+
+# find the best model ----------------------------------------------------------
+
+# save only the parameters
+best_param = cv.best_params_
+write_cp(best_param, 'best_param.pkl')
+
---
+++ feature_names.py
@@ -0,0 +1,70 @@
+
+# based on the code from Kyle Glide
+# https://www.kaggle.com/code/kylegilde/extracting-scikit-feature-names-importances/notebook
+#
+# updated
+# - add a condition to check get_feature_names_out() first
+#    https://github.com/scikit-learn/scikit-learn/issues/12525#issuecomment-1071203398
+# - Modified to remove the dependency on plotly 
+# - exract only one function
+# by Taekyun Kim (taekyunk@gmail.com)
+
+import numpy as np  
+import pandas as pd  
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.utils.validation import check_is_fitted
+
+
+def get_feature_names(column_transformer):
+    """
+    Get feature names from a fitted column transformer
+    """
+    assert isinstance(column_transformer, ColumnTransformer), "Input isn't a ColumnTransformer"
+    check_is_fitted(column_transformer)
+
+    new_feature_names = []
+
+    for i, transformer_item in enumerate(column_transformer.transformers_): 
+        transformer_name, transformer, orig_feature_names = transformer_item
+        orig_feature_names = list(orig_feature_names)
+        
+        if transformer == 'drop':
+            continue
+            
+        if isinstance(transformer, Pipeline):
+            # if pipeline, get the last transformer in the Pipeline
+            transformer = transformer.steps[-1][1]
+
+        if hasattr(transformer, 'get_feature_names_out'):
+            if 'input_features' in transformer.get_feature_names_out.__code__.co_varnames:
+                names = list(transformer.get_feature_names_out(orig_feature_names))
+            else:
+                names = list(transformer.get_feature_names_out())
+
+        elif hasattr(transformer, 'get_feature_names'):
+            if 'input_features' in transformer.get_feature_names.__code__.co_varnames:
+                names = list(transformer.get_feature_names(orig_feature_names))
+            else:
+                names = list(transformer.get_feature_names())
+
+        elif hasattr(transformer,'indicator_') and transformer.add_indicator:
+            # is this transformer one of the imputers & did it call the MissingIndicator?
+            missing_indicator_indices = transformer.indicator_.features_
+            missing_indicators = [orig_feature_names[idx] + '_missing_flag'\
+                                    for idx in missing_indicator_indices]
+            names = orig_feature_names + missing_indicators
+
+        elif hasattr(transformer,'features_'):
+            # is this a MissingIndicator class? 
+            missing_indicator_indices = transformer.features_
+            missing_indicators = [orig_feature_names[idx] + '_missing_flag'\
+                                    for idx in missing_indicator_indices]
+
+        else:
+            names = orig_feature_names
+
+        new_feature_names.extend(names)
+
+    return new_feature_names
+
